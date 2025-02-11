937cee3ea7221f317d170b8003deb0d6fdb5cfdb
Sieun Kim <20210760@dongduk.ac.kr>
2025-02-12 00:19:39 +0900

    Initial Commit

---
+++ src/main/java/com/moviediary/backend/MovieDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.moviediary.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moviediary/backend/MovieDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviediary.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
